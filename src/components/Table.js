4971302e17f60396213c4712d6c4745302efc259
thebasedtaka <darktaka9998@gmail.com>
2023-03-15 23:18:15 -0400

    table update

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,31 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((rowdata) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowdata)}
+        </td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(rowdata)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -7,9 +7,23 @@ function TablePage() {
     { name: "Bananna", color: "bg-yellow-500", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
   ];
+
+  const config = [
+    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+    },
+    { label: "Score", render: (fruit) => fruit.score },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+
   return (
     <div>
-      <Table />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
