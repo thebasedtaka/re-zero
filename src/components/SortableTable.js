cc8bf9218155c463e7cd1ef4bf3bac19f8a0d281
thebasedtaka <darktaka9998@gmail.com>
2023-03-21 20:29:02 -0400

    immer update

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,31 +1,14 @@
 import Table from "./Table";
 import { useState } from "react";
 import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
+import useSort from "../hooks/use-sort";
 
 function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-
   const { config, data } = props;
-
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortOrder("asc");
-      setSortBy(label);
-      return;
-    }
-
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  const { sortBy, sortOrder, sortedData, setSortColumn } = useSort(
+    config,
+    data
+  );
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
@@ -37,7 +20,7 @@ function SortableTable(props) {
       header: () => (
         <th
           className="cursor-pointer hover:bg-gray-100"
-          onClick={() => handleClick(column.label)}
+          onClick={() => setSortColumn(column.label)}
         >
           <div className="flex items-center">
             {getIcons(column.label, sortBy, sortOrder)}
@@ -47,25 +30,6 @@ function SortableTable(props) {
       ),
     };
   });
-
-  //Only sort data if sortOrder && sortBy !null
-  //Make copy of data prop
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
 
   return (
     <div>
--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,31 +1,80 @@
-import { useState, useEffect } from "react";
+import produce from "immer";
+import userEvent from "@testing-library/user-event";
+import { useReducer } from "react";
 import Button from "../components/Button";
+import Panel from "../components/Panel";
 
-function useLogCount(initialCount) {
-  const [count, setCount] = useState(initialCount);
+const INCREMENT_COUNT = "increment-count";
+const SET_VALUE_TO_ADD = "change_value_to_add";
+const DECREMENT_COUNT = "decrement-count";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
 
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  return {
-    count,
-    increment,
-  };
-}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count = state.count + 1;
+      return;
+    case DECREMENT_COUNT:
+      state.count = state.count - 1;
+      return;
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
+    case SET_VALUE_TO_ADD:
+      state.valueToAdd = action.payload;
+      return;
+    default:
+      return state;
+  }
+};
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useLogCount(initialCount);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, SetValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(produce(reducer), {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+  const increment = () => {
+    dispatch({ type: INCREMENT_COUNT });
+  };
+  const decrement = () => {
+    dispatch({ type: DECREMENT_COUNT });
+  };
+
+  const handleChange = (e) => {
+    //e.preventDefault();
+    const value = +e.target.value;
+    console.log(value);
+    dispatch({ type: SET_VALUE_TO_ADD, payload: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch({ type: ADD_VALUE_TO_COUNT });
+  };
 
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {state.count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>increment</Button>
+        <Button onClick={decrement}>decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add things</label>
+        <input
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add It</Button>
+      </form>
+    </Panel>
   );
 }
 
