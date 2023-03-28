a88ebd3fc797c21408f43de9331d5c97363eb451
thebasedtaka <thebasedtaka@CodeVM.myguest.virtualbox.org>
2023-03-28 19:19:38 -0400

    final carform

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    data: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    // Assuming action.payload === {name:'name', cost: 69}
+    addCar(state, action) {
+      state.data.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    },
+    // Assuming action.payload === id of removed car
+    removeCar(state, action) {
+      const updated = state.data.filter((car) => {
+        return car.id !== action.payload;
+      });
+      state.data = updated;
+    },
+  },
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
